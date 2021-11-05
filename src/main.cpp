83cb6cd4c0752788613e59a145f9ddf7c3dc7dec
Tomas Tisocco <tomastisocco97@gmail.com>
2021-11-04 21:55:59 -0300

    setup proyect

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,58 @@
 #include <Arduino.h>
 
+typedef union{
+  struct{
+    uint8_t b0:1;
+    uint8_t b1:1;
+    uint8_t b2:1;
+    uint8_t b3:1;
+    uint8_t b4:1;
+    uint8_t b5:1;
+    uint8_t b6:1;
+    uint8_t b7:1;
+  }bit;
+  uint8_t byte;
+}_flag;
+
+// Definicion Variables de Entrada
+
+#define SENSORENTRADA1 2
+#define SENSORENTRADA2 3
+
+#define SENSORSALIDA1 4
+#define SENSORSALIDA2 5
+#define SENSORTARJETA 6
+
+#define BOTONM 7
+#define BOTONR 8
+#define BOTONP 9
+
+//Definicion Variables de Salida
+
+#define LUZROJA 10
+#define LUZVERDE 11
+
+#define APERTURAENTRADA 12
+#define CIERREENTRADA 13
+#define APERTURASALIDA 14
+#define CIERRESALIDA 15
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(SENSORENTRADA1, INPUT);
+  pinMode(SENSORENTRADA2, INPUT);
+  pinMode(SENSORSALIDA1, INPUT);
+  pinMode(SENSORSALIDA2, INPUT);
+  pinMode(SENSORTARJETA, INPUT);
+  pinMode(BOTONM, INPUT);
+  pinMode(BOTONR, INPUT);
+  pinMode(BOTONP, INPUT);
+
+  pinMode(LUZVERDE, OUTPUT);
+  pinMode(LUZROJA, OUTPUT);
+  pinMode(APERTURAENTRADA, OUTPUT);
+  pinMode(CIERREENTRADA, OUTPUT);
+  pinMode(APERTURASALIDA, OUTPUT);
+  pinMode(CIERRESALIDA, OUTPUT);
 }
 
 void loop() {
